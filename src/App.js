918cb71a8a20987cca2fe64d8848a3a229597008
sumitshinde-84 <shindesumit217@gmail.com>
2023-04-21 21:36:58 +0530

    add footer componet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useState,useEffect } from "react";
 import Header from "./component/header";
+import Footer from "./component/footer";
 import "./App.css"
 const App = () =>{
   return(
     <div className="main">
       <Header/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/component/footer.js
@@ -0,0 +1,12 @@
+import "./styles/footer.css"
+const Footer =()=>{
+    return(
+        <div className="footer">
+            <a href="https://github.com/sumitshinde-84">Copyright © {new Date().getFullYear()} sumitshinde-84</a>
+        <img src="../images/github.png"/>
+        </div>
+
+    )
+}
+
+export default Footer;
