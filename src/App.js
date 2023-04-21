487f2191be6726ba2c7846b9c95c5c7aca8daee9
sumitshinde-84 <shindesumit217@gmail.com>
2023-04-22 01:18:37 +0530

    add loader compnent

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import Header from "./component/header";
 import Footer from "./component/footer";
 import "./App.css"
 import Container from "./component/container";
+import Loader from "./component/loader";
 const App = () =>{
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      document.querySelector('section').style.display="none"
+    },5000)
+
+  },[])
+
+
   return(
     <div className="main">
+      <Loader/>
       <Header/>
       <Container/>
       <Footer/>
---
+++ src/component/loader.js
@@ -0,0 +1,11 @@
+import "./styles/loader.css";
+import loaderImg from "../images/loader.gif";
+const Loader = () => {
+  return (
+    <section>
+      <img src={loaderImg} alt="loader" />
+    </section>
+  );
+};
+
+export default Loader;
